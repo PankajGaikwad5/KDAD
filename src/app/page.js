4f917a6d1c8840767da7acd7441156f9d505cdfd
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-01-22 17:17:41 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,37 @@
+'use client';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Image from 'next/image';
 import LogoComp from '../components/LogoComp';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [gifKey, setGifKey] = useState('');
+  const [logoLoaded, setLogoLoaded] = useState(false);
+
+  useEffect(() => {
+    // Generate a unique key (timestamp) on every page load
+    setGifKey(`?reload=${Date.now()}`);
+    setInterval(() => {
+      setLogoLoaded(true);
+    }, 4000);
+  }, []);
   return (
     // <div className='relative overflow-hidden'>
     <div className='relative w-full h-screen overflow-hidden m-0 p-0'>
       <Navbar isBgBlack={true} isHomePage={true} />
-      <div className='absolute inset-0 z-0 bg-cover bg-center  animate-bg-change'></div>
+      <div
+        className={`absolute inset-0 z-0 bg-cover bg-center ${
+          !logoLoaded ? 'opacity-10' : 'opacity-65'
+        } animate-bg-change transition-all duration-500`}
+      ></div>
       <div className='relative z-10 font-semibold text-center top-1/2 -mt-12 lg:-mt-2 -translate-y-1/2 text-3xl sm:text-8xl text-white tracking-widest flex flex-col  justify-center items-center mb-0'>
         {/* <h1 className='text-center'>karan desai</h1>
         <h1 className='text-2xl font-sans text-center uppercase font-thin linespace '>
           architecture + design
         </h1> */}
-        <img src='/assets/KD_signature.gif' alt='' />
+        {/* <img src='/assets/signature.gif' alt='' /> */}
+        <img src={`/assets/signature.gif${gifKey}`} alt='Signature Animation' />
       </div>
       {/* <div className='absolute right-0 top-0 -mt-12 z-30'>
         <Image
