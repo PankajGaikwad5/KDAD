cac8fae885d1ee40cc3301202f6335cf5c9a699b
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-01-24 19:32:43 +0530

    update

--- src/app/about%20us/page.js
+++ src/app/about%20us/page.js
@@ -14,9 +14,7 @@ const About = () => {
             img={'/assets/profile.JPG'}
             imagePosition={'left'}
             title={'Karan Desai'}
-            desc={
-              'Architecture + Design | Award Winning Architect | TedX Speaker'
-            }
+            desc={'Award Winning Architecture + Interior Design | TedX Speaker'}
             text={`Born in 1987, a passionate founder of his eponymous studio, KARAN DESAI | Architecture + Design, focusing on Architecture, Interiors & furniture designing, KD started off with his individual practice right after he gave his Thesis in 2011 from Pillai’s college of architecture & founded the company in 2012. The internship under Ar. Ashiesh Shah during a year drop in 2007, carved a path for his career with a clear direction towards his goals & dreams which he lives today. The Studio has spread its wings in Mangalore, Goa, Delhi, Kullu - Manali, Uttarakhand, Kolkata, Chennai and plan to continue. Inspired by contemporary aesthetics and clean lines, the studio beautifies projects both residential and commercial on varying scales. From ideation rooms to offices , homes to private getaways, the team designs projects and products in close association with clients to deliver unique results and reflect personal tastes with consolidating the studio’s vision
 `}
           />
--- src/app/page.js
+++ src/app/page.js
@@ -18,28 +18,29 @@ export default function Home() {
     }, 3500);
   }, []);
   return (
-    // <div className='relative overflow-hidden'>
-    <div className='relative w-full h-screen overflow-hidden m-0 p-0'>
-      <CursorTrail />
-      <Navbar isBgBlack={true} isHomePage={true} />
-      <div
-        className={`absolute inset-0 z-0 bg-cover bg-center ${
-          !logoLoaded ? 'opacity-20' : 'opacity-90'
-        } animate-bg-change transition-all duration-700`}
-      ></div>
-      <div className='relative z-10 font-semibold text-center top-1/2 -mt-24 lg:-mt-8 -translate-y-1/2 text-3xl sm:text-8xl text-white tracking-widest flex flex-col  justify-center items-center mb-0'>
-        {/* <h1 className='text-center'>karan desai</h1>
+    <div className='bg-black/90'>
+      {/* // <div className='relative overflow-hidden'> */}
+      <div className='relative w-full h-screen overflow-hidden m-0 p-0'>
+        <CursorTrail />
+        <Navbar isBgBlack={true} isHomePage={true} />
+        <div
+          className={`absolute inset-0 z-0 bg-cover bg-center ${
+            !logoLoaded ? 'opacity-20' : 'opacity-90'
+          } animate-bg-change transition-all duration-700`}
+        ></div>
+        <div className='relative z-10 font-semibold text-center top-1/2 -mt-24 lg:-mt-8 -translate-y-1/2 text-3xl sm:text-8xl text-white tracking-widest flex flex-col  justify-center items-center mb-0'>
+          {/* <h1 className='text-center'>karan desai</h1>
         <h1 className='text-2xl font-sans text-center uppercase font-thin linespace '>
           architecture + design
         </h1> */}
-        {/* <img src='/assets/signature.gif' alt='' /> */}
-        <img
-          src={`/assets/sign.gif${gifKey}`}
-          className={`${!logoLoaded ? 'opacity-100' : 'opacity-30'}`}
-          alt='Signature Animation'
-        />
-      </div>
-      {/* <div className='absolute right-0 top-0 -mt-12 z-30'>
+          {/* <img src='/assets/signature.gif' alt='' /> */}
+          <img
+            src={`/assets/sign.gif${gifKey}`}
+            className={`${!logoLoaded ? 'opacity-100' : 'opacity-30'}`}
+            alt='Signature Animation'
+          />
+        </div>
+        {/* <div className='absolute right-0 top-0 -mt-12 z-30'>
         <Image
           src='/assets/newlogo.png'
           alt='logo'
@@ -48,12 +49,13 @@ export default function Home() {
           className='object-contain'
         />
       </div> */}
-      <div className='w-full fixed left-[50%] bottom-[2%] translate-x-[-50%]  flex items-center text-black px-4  overflow-hidden z-10 m-0'>
-        <LogoComp />
+        <div className='w-full fixed left-[50%] bottom-[2%] translate-x-[-50%]  flex items-center text-black px-4  overflow-hidden z-10 m-0'>
+          <LogoComp />
 
-        <Footer />
+          <Footer />
+        </div>
+        //{' '}
       </div>
-      //{' '}
     </div>
   );
 }
