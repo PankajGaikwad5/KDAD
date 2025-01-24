b0bd7b4930773ef7146f2315041ea4f560dc3991
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-01-24 17:43:45 +0530

    update

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -137,7 +137,7 @@ const CarouselComp = ({ imgArray }) => {
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         zoom={true}
-        keyboard={{ enabled: true, onlyInViewport: true }}
+        keyboard={{ enabled: true }}
       >
         {imgArray.map((img, index) => (
           <SwiperSlide key={index}>
