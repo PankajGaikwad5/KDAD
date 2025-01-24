113d6289c601b8d5eca70e5653778119e3e26750
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-01-24 15:04:45 +0530

    update

--- src/app/features/page.js
+++ src/app/features/page.js
@@ -11,7 +11,7 @@ const Features = () => {
     // Fetch topics from the API
     const fetchProjects = async () => {
       try {
-        const response = await fetch(`${process.env.BASE_URL}/api/features`);
+        const response = await fetch(`/api/features`);
         const data = await response.json();
         console.log(data);
 
