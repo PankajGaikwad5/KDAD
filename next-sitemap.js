f2a31fb299a33f9727d934c3d8c9bc4b7479af5a
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-02-27 17:28:02 +0530

    update

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: 'https://www.karandesai.in/', // Replace with your domain
   generateRobotsTxt: true, // Generates robots.txt with sitemap link
   sitemapSize: 5000, // Adjust if you have more URLs
+  outDir: './public',
 };
